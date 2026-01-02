0fd486ba05546b8d3d3186f6f548a5887a501aef
Amichai Buskila <amichai.buskila@gmail.com>
2026-01-02 10:40:53 +0200

    add a content in about page

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -4,11 +4,16 @@ import Link from 'next/link'
 export default function Home() {
   return (
     <div className="page-container">
-      <h1>about</h1>
       <div class className="navbar">
         <nav>
           [<Link href="/">Home</Link> | <Link href="/about">About</Link>]
         </nav>
+      </div>
+      <div className="content">
+        <h1>About This Application</h1>
+        <p>
+          hi there! This application is built using Next.js, a powerful React framework for building web applications. It demonstrates the use of routing, components, and server-side rendering.
+        </p>
       </div>
     </div>
   );
