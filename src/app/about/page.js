57d022a86ca9c808c00cec597d6271b5641f6c26
Amichai Buskila <amichai.buskila@gmail.com>
2026-01-09 10:15:49 +0200

    add a counter

--- src/app/about/page.js
+++ src/app/about/page.js
@@ -1,12 +1,25 @@
+'use client';
+
+import React, { useState } from 'react';
+
 export default function Home() {
+  const [count, setCount] = useState(0);
+
+  function handleClick() {
+    setCount(count + 1);
+  }
+
   return (
-    
-        <div className="content">
-          <h1>About This Application</h1>
-          <p>
-            hi my name is amichai and i made this app to help people learn more about web development and programming. This application provides resources, tutorials, and examples to assist users in their learning journey.
-          </p>
-      </div>
+
+    <div className="content">
+      <h1>About This Application</h1>
+      <p>
+        hi my name is amichai and i made this app to help people learn more about web development and programming. This application provides resources, tutorials, and examples to assist users in their learning journey.
+      </p>
+      <button onClick={handleClick}> Click Me</button>
+      <br />
+      {count}
+    </div>
 
   );
 }
